249e0760c8ffd8e5c76cdfa84cb66ce355dad017
Agustin Velazquez <agubolso2@gmail.com>
2025-01-25 05:46:08 -0300

    add railway config

--- config/postgresClient.js
+++ config/postgresClient.js
@@ -1,12 +1,8 @@
 const { Pool } = require("pg");
 
-// Configuración del Pool de conexiones
+// Configuración del Pool de conexiones usando la variable de entorno POSTGRES_URL
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "todolist",
-  password: "123456",
-  port: 5433, // Puerto predeterminado de PostgreSQL
+  connectionString: process.env.POSTGRES_URL, // Usar la URL de conexión proporcionada por Railway
 });
 
 // Manejo de errores
